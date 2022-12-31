8f4dcf0a792085ca383ec4395b1f38159b7909db
jzf21 <jozefneelamkavil2003@gmail.com>
2022-12-31 12:56:00 +0530

    more changes

--- Calculator.java
+++ Calculator.java
@@ -1,42 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Calculator extends JFrame {
-    public Calculator() {
-        super("Calculator");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLookAndFeel();
-        setSize(300, 200);
-        setVisible(true);
+public class Calculator {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(new GridLayout(4, 4));
 
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+        JButton button0 = new JButton("0");
+        JButton buttonPlus = new JButton("+");
+        JButton buttonMinus = new JButton("-");
+        JButton buttonMultiply = new JButton("*");
+        JButton buttonDivide = new JButton("/");
+        JButton buttonEquals = new JButton("=");
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+        frame.add(button7);
+        frame.add(button8);
+        frame.add(button9);
+        frame.add(button0);
+        frame.add(buttonPlus);
+        frame.add(buttonMinus);
+        frame.add(buttonMultiply);
+        frame.add(buttonDivide);
+        frame.add(buttonEquals);
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("1");
+            }
+        });
+        frame.setVisible(true);
     }
 
-    private static void setLookAndFeel() {
-        try {
-            UIManager.setLookAndFeel(
-                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
-            );
-        } catch (Exception exc) {
-            // ignore error
-        }
-    }
-
-    public static void main(String[] arguments) {
-        Calculator.setLookAndFeel();
-        Calculator calc = new Calculator();
-    }
-}
-
-class OperatorButtons extends JPanel {
-    public OperatorButtons() {
-        JButton add = new JButton("+");
-        JButton sub = new JButton("-");
-        JButton mul = new JButton("*");
-        JButton div = new JButton("/");
-        JButton equ = new JButton("=");
-        add(add);
-        add(sub);
-        add(mul);
-        add(div);
-        add(equ);
-    }
 }
